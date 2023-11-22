63048bef5c266965e8970c058c2766b0e65fd7ab
abenaya08 <abenaya082004@gmail.com>
2023-11-22 09:44:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-//App.js
 import './App.css';
-import DeleteAxios from './Day8/DeleteAxios';
-//import AxiosGet from './Day8/AxiosGet';
-import PostAxios from './Day8/PostAxios';
-
-function App() {
+import {useState} from 'react';
+const App = () => {
+  const [su,setsu] =useState(true);
+  const handlesu=()=>{
+    setsu(!su)
+  }
   return (
-    <div className="App">
-      <PostAxios/>
-      <DeleteAxios/>
-    </div>
-  );
+   <>
+      <body>
+       
+        
+        <button  className={su?'log':'log1'} onClick={handlesu}>SHUT</button>
+      
+       
+       <div className="container2">
+        <form>
+
+        <input  type='text' className='uname1' placeholder=' user name'/>
+        <input type='password' className='password1' placeholder=' password' required/>
+        <input type='password' className='conpassword' placeholder=' confirm password'/>
+        <button className='sibutton'>SIGN UP</button>
+        </form>
+        <button  onClick={handlesu} className={su?'sub':'sub1'}>SIGN UP</button>
+         <div className={su?'sur':'sul'} ></div>
+       </div>
+      </body>
+   </>
+  )
 }
 
-export default App;
+export default App;
---
+++ src/SignUpPage/SignUpPage.js
@@ -0,0 +1,13 @@
+// src/components/SignUpPage.js
+import React from 'react';
+import SignUp from './signupDetail';
+const SignUpPage = () => {
+  return (
+    <div>
+      <h2>Sign Up Here!</h2>
+      <SignUp/>
+    </div>
+  );
+};
+
+export default SignUpPage;
